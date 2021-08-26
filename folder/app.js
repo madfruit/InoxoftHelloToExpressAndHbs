1f51a64f24e0ebb60f693249a565047b9e9d22d4
Ivan Fedchenkov <masdfruitbest@gmail.com>
2021-08-26 15:29:55 +0300

    The third task is ready

--- folder/app.js
+++ folder/app.js
@@ -1,17 +1,11 @@
-const express = require("express");
-const expressHbs = require("express-handlebars");
-const path = require("path");
-const fs = require("fs");
-const util = require("util");
+const express = require('express');
+const expressHbs = require('express-handlebars');
+const path = require('path');
 
-const writeFilePromisified = util.promisify(fs.writeFile);
-
-const { PORT } = require("./configs/config");
+const { PORT } = require('./configs/config');
 
 const app = express();
 const staticPath = path.join(__dirname, 'static');
-const databasePath = path.join(__dirname, "database/users.json");
-const users = require(databasePath);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,56 +14,15 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', expressHbs({ defaultLayout: false }));
 app.set('views', staticPath);
 
+const { authRouter, userRouter } = require('./routers');
+
+app.use('/', authRouter);
+app.use('/users', userRouter);
+
 app.get('/', (req, res) => {
     res.redirect('/auth');
 });
 
-app.get('/auth', (req, res) => {
-    res.render('login');
-});
-
-app.post('/auth', (req, res) => {
-    const { email, password } = req.body;
-
-    const user = users.find(user => user.email === email);
-
-    if (!user) {
-        res.redirect('/register');
-        return;
-    }
-    if (password !== user.password) {
-        res.status(403).end('Access denied, wrong password');
-    }
-    res.redirect(`users/${user.email}`);
-});
-
-app.get('/users/:user_email', (req, res) => {
-    const { user_email } = req.params;
-    const user = users.find(user => user.email == user_email);
-    res.render('user', { user });
-});
-
-app.get('/users', (req, res) => {
-    res.render('users', { users });
-});
-
-app.get('/register', (req, res) => {
-    res.render('register')
-});
-
-app.post('/register', (req, res) => {
-    const user = req.body;
-    const userByEmail = users.find(u => u.email == user.email);
-    if (userByEmail) {
-        res.end('Email is already in use');
-        return;
-    }
-    users.push(user);
-    writeFilePromisified(databasePath, JSON.stringify(users)).then(() => {
-        res.redirect('/auth');
-    });
-});
-
 app.listen(PORT, () => {
-    console.log("listening on port", PORT);
+    console.log('listening on port', PORT);
 });
